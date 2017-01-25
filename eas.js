3931220413e5f309b45a768a5ad86410667fb335
Panoptes71 <pjabram@yahoo.co.uk>
2017-01-25 15:17:26 +0000

    Added html and js

---
+++ eas.js
@@ -0,0 +1,32 @@
+gridSize=16;
+
+$(document).ready(function() {
+	createGrid(gridSize);
+});
+
+function createGrid(gridSize) {
+
+	$('#grid').empty();
+	var gridBoxes = gridSize * gridSize;
+
+	var boxSize = (400/gridSize)-2;
+
+	for ( var i = 0; i < gridBoxes; i++ ) {
+		$('#grid').append("<div class='unit' style='height:" + boxSize + "px; width:" + boxSize + "px;'></div>");
+			}
+
+			hoverEffects();
+};
+
+function hoverEffects(){
+
+	$('.unit').hover (
+		function() {
+			$(this).addClass("hover");
+		},
+		function() {
+			$(this).removeClass("hover");
+			$(this).css("background-color", "green");
+		}
+	)
+};
